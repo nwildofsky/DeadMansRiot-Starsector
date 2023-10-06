99b226204d462624ac77ff7099b38552ef37345c
dcc6557 <dcc6557@rit.edu>
2023-10-06 14:02:21 -0400

    Internal Affairs Mod Appears in-game
    Will work on gutting the files so we can put in our mission info.

--- JavaWorkspace/src/scripts/campaign/intel/missions/Riot.java
+++ JavaWorkspace/src/scripts/campaign/intel/missions/Riot.java
@@ -16,7 +16,6 @@ import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -55,35 +54,31 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
         // a simple boolean we'll sometimes flip on and do extra stuff
         boolean remnant = false;
 
-        // if bar event, let's create a person to actually give it to us
-        if (barEvent) {
-            setGiverRank(Ranks.CITIZEN);
-            setGiverPost(Ranks.POST_CITIZEN);
-            setGiverImportance(PersonImportance.LOW);
-            setGiverFaction(Factions.POOR);
-            setGiverTags(Tags.CONTACT_MILITARY);
-            setGiverVoice(Voices.SPACER);
-            findOrCreateGiver(createdAt, false, false);
-        }
+
+        setGiverRank(Ranks.AGENT);
+        setGiverPost(Ranks.POST_EXECUTIVE);
+        setGiverImportance(PersonImportance.HIGH);
+        setGiverFaction(Factions.TRITACHYON);
+        setGiverTags(Tags.CONTACT_MILITARY);
+        setGiverVoice(Voices.BUSINESS);
+        findOrCreateGiver(createdAt, false, false);
 
         PersonAPI person = getPerson();
         if (person == null) return false;
 
         // setting the mission ref allows us to use the Call rulecommand in their dialogues, so that we can make this script do things
-        if (!setPersonMissionRef(person, "$riot_ref")) {
+        if (!setPersonMissionRef(person, "$intaff_ref")) {
             return false;
         }
 
-        // when we complete the mission from a bar event, always allow us to add the questgiver as a contact
-        if (barEvent) {
-            setGiverIsPotentialContactOnSuccess(1f);
-        }
+
+        setGiverIsPotentialContactOnSuccess(1f);
 
         // set up the disgraced executive
         executive = Global.getSector().getFaction(Factions.TRITACHYON).createRandomPerson();
         executive.setRankId(Ranks.SPACE_ADMIRAL);
         executive.setPostId(Ranks.POST_SENIOR_EXECUTIVE);
-        executive.getMemoryWithoutUpdate().set("$riot_exec", true);
+        executive.getMemoryWithoutUpdate().set("$intaff_exec", true);
 
         // pick the system with the clues inside
         requireSystemInterestingAndNotUnsafeOrCore();
@@ -103,17 +98,26 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
         system2 = pickSystem(true);
         if (system2 == null) return false;
 
+        // roll to see if we're adding Remnants
+        if (rollProbability(PROB_REMNANT)) {
+            remnant = true;
+        }
+
         // determine the faction and ship type of the derelict
         String derelict_faction = Factions.TRITACHYON;
         DerelictType derelict_type = DerelictType.CIVILIAN;
+        if (remnant) {
+            derelict_faction = Factions.REMNANTS;
+            derelict_type = DerelictType.SMALL;
+        }
 
         // spawn a supply cache and derelict ship, both serving as clues. They have memory flags that are checked for in rules.csv
         cache = spawnEntity(Entities.SUPPLY_CACHE, new LocData(EntityLocationType.HIDDEN, null, system));
         derelict = spawnDerelict(derelict_faction, derelict_type, new LocData(EntityLocationType.HIDDEN, null, system));
-        cache.getMemoryWithoutUpdate().set("$riot_clue", true);
-        setEntityMissionRef(cache, "$riot_ref");
-        derelict.getMemoryWithoutUpdate().set("$riot_clue", true);
-        setEntityMissionRef(derelict, "$riot_ref");
+        cache.getMemoryWithoutUpdate().set("$intaff_clue", true);
+        setEntityMissionRef(cache, "$intaff_ref");
+        derelict.getMemoryWithoutUpdate().set("$intaff_clue", true);
+        setEntityMissionRef(derelict, "$intaff_ref");
 
         // set up the target fleet. I've done this using the old style, because the trigger-system doesn't support event listeners by default,
         // and we need to know when this fleet dies or despawns
@@ -142,15 +146,15 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
         target.getFlagship().setCaptain(executive);
 
         Misc.makeHostile(target);
-        Misc.makeNoRepImpact(target, "$riot");
-        Misc.makeImportant(target, "$riot");
+        Misc.makeNoRepImpact(target, "$intaff");
+        Misc.makeImportant(target, "$intaff");
 
-        target.getMemoryWithoutUpdate().set(MemFlags.MEMORY_KEY_MAKE_AGGRESSIVE, "$riot");
-        target.getMemoryWithoutUpdate().set(MemFlags.MEMORY_KEY_MAKE_AGGRESSIVE_ONE_BATTLE_ONLY, "$riot");
-        target.getMemoryWithoutUpdate().set(MemFlags.FLEET_IGNORED_BY_OTHER_FLEETS, "$riot");
-        target.getMemoryWithoutUpdate().set(MemFlags.FLEET_IGNORES_OTHER_FLEETS, "$riot");
+        target.getMemoryWithoutUpdate().set(MemFlags.MEMORY_KEY_MAKE_AGGRESSIVE, "$intaff");
+        target.getMemoryWithoutUpdate().set(MemFlags.MEMORY_KEY_MAKE_AGGRESSIVE_ONE_BATTLE_ONLY, "$intaff");
+        target.getMemoryWithoutUpdate().set(MemFlags.FLEET_IGNORED_BY_OTHER_FLEETS, "$intaff");
+        target.getMemoryWithoutUpdate().set(MemFlags.FLEET_IGNORES_OTHER_FLEETS, "$intaff");
 
-        target.getMemoryWithoutUpdate().set("$riot_execfleet", true);
+        target.getMemoryWithoutUpdate().set("$intaff_execfleet", true);
         target.getAI().addAssignment(FleetAssignment.PATROL_SYSTEM, system2.getCenter(), 200f, null);
         target.addEventListener(this);
         system2.addEntity(target);
@@ -170,7 +174,7 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
         }
 
         // set a global reference we can use, useful for once-off missions.
-        if (!setGlobalReference("$riot_ref")) return false;
+        if (!setGlobalReference("$intaff_ref")) return false;
 
         // set our starting, success and failure stages
         setStartingStage(Stage.FIND_CLUE);
@@ -178,9 +182,9 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
         setFailureStage(Stage.FAILED);
 
         // set stage transitions when certain global flags are set, and when certain flags are set on the questgiver
-        setStageOnGlobalFlag(Stage.KILL_FLEET, "$riot_foundclue");
-        setStageOnMemoryFlag(Stage.COMPLETED, person, "$riot_completed");
-        setStageOnMemoryFlag(Stage.FAILED, person, "$riot_failed" );
+        setStageOnGlobalFlag(Stage.KILL_FLEET, "$intaff_foundclue");
+        setStageOnMemoryFlag(Stage.COMPLETED, person, "$intaff_completed");
+        setStageOnMemoryFlag(Stage.FAILED, person, "$intaff_failed" );
         // set time limit and credit reward
         setTimeLimit(Stage.FAILED, MISSION_DAYS, system2);
         setCreditReward(CreditReward.HIGH);
@@ -189,34 +193,35 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
     }
 
     // when Call-ing something that isn't a default option for a mission, it'll try and run this method with "action" being the first parameter
-    // e.g Call $global.riot_ref unsetClues
+    // e.g Call $global.intaff_ref unsetClues
     @Override
     protected boolean callAction(String action, String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
         if (action.equals("unsetClues")){
             // make the other clue no longer a clue
             if (cache != null) {
-                cache.getMemoryWithoutUpdate().unset("$riot_clue");
+                cache.getMemoryWithoutUpdate().unset("$intaff_clue");
             }
             if (derelict != null) {
-                derelict.getMemoryWithoutUpdate().unset("$riot_clue");
+                derelict.getMemoryWithoutUpdate().unset("$intaff_clue");
             }
             return true;
         }
         return false;
     }
 
-    // during the initial dialogue and in any dialogue where we use "Call $riot_ref updateData", these values will be put in memory
-    // here, used so we can, say, type $riot_execName and automatically insert the disgraced executive's name
+    // during the initial dialogue and in any dialogue where we use "Call $intaff_ref updateData", these values will be put in memory
+    // here, used so we can, say, type $intaff_execName and automatically insert the disgraced executive's name
     protected void updateInteractionDataImpl() {
-        set("$riot_barEvent", isBarEvent());
-        set("$riot_manOrWoman", getPerson().getManOrWoman());
-        set("$riot_heOrShe", getPerson().getHeOrShe());
-        //set("$riot_reward", Misc.getWithDGS(getCreditsReward()));
+        set("$intaff_barEvent", isBarEvent());
+        set("$intaff_manOrWoman", getPerson().getManOrWoman());
+        set("$intaff_heOrShe", getPerson().getHeOrShe());
+        set("$intaff_reward", Misc.getWithDGS(getCreditsReward()));
 
-        set("$riot_personName", getPerson().getNameString());
-        set("$riot_systemName", system.getNameWithLowercaseTypeShort());
-        set("$riot_system2Name", system2.getNameWithLowercaseTypeShort());
-        set("$riot_dist", getDistanceLY(system));
+        set("$intaff_personName", getPerson().getNameString());
+        set("$intaff_execName", executive.getNameString());
+        set("$intaff_systemName", system.getNameWithLowercaseTypeShort());
+        set("$intaff_system2Name", system2.getNameWithLowercaseTypeShort());
+        set("$intaff_dist", getDistanceLY(system));
     }
 
     // used to detect when the executive's fleet is destroyed and complete the mission
@@ -230,7 +235,7 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
         if (battle.isInvolved(fleet) && !playerInvolved) {
             if (fleet.getFlagship() == null || fleet.getFlagship().getCaptain() != target) {
                 fleet.setCommander(fleet.getFaction().createRandomPerson());
-                getPerson().getMemoryWithoutUpdate().set("$riot_completed", true);
+                getPerson().getMemoryWithoutUpdate().set("$intaff_completed", true);
                 return;
             }
         }
@@ -242,7 +247,7 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
         // didn't destroy the original flagship
         if (fleet.getFlagship() != null && fleet.getFlagship().getCaptain() == target) return;
 
-        getPerson().getMemoryWithoutUpdate().set("$riot_completed", true);
+        getPerson().getMemoryWithoutUpdate().set("$intaff_completed", true);
 
     }
 
@@ -250,8 +255,8 @@ public class Riot extends HubMissionWithBarEvent implements FleetEventListener {
     public void reportFleetDespawnedToListener(CampaignFleetAPI fleet, CampaignEventListener.FleetDespawnReason reason, Object param) {
         if (isDone() || result != null) return;
 
-        if (fleet.getMemoryWithoutUpdate().contains("$riot_execfleet")) {
-            getPerson().getMemoryWithoutUpdate().set("$riot_failed", true);
+        if (fleet.getMemoryWithoutUpdate().contains("$intaff_execfleet")) {
+            getPerson().getMemoryWithoutUpdate().set("$intaff_failed", true);
         }
     }
 

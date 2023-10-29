214efd06ccebcccbea44caf08a7461f880d2dde7
JTyson02 <70408537+JTyson02@users.noreply.github.com>
2023-10-29 12:58:55 -0400

    Quick change to Berserk stats
    Boosted the range and projectile hp by 75%

--- JavaWorkspace/src/data/missions/berserk_mission/MissionDefinition.java
+++ JavaWorkspace/src/data/missions/berserk_mission/MissionDefinition.java
@@ -16,6 +16,7 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 			api.addToFleet(FleetSide.PLAYER, "aurora_Balanced", FleetMemberType.SHIP, true);
 			api.addToFleet(FleetSide.PLAYER, "medusa_CS", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.PLAYER, "harbinger_Strike", FleetMemberType.SHIP, false);
+			api.addToFleet(FleetSide.PLAYER, "harbinger_Strike", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.PLAYER, "shrike_Support", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.PLAYER, "shrike_Attack", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.PLAYER, "wolf_PD", FleetMemberType.SHIP, false);
@@ -24,6 +25,8 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 			
 			api.addToFleet(FleetSide.ENEMY, "venture_pather_Attack", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "colossus2_Pather", FleetMemberType.SHIP, false);
+			api.addToFleet(FleetSide.ENEMY, "colossus2_Pather", FleetMemberType.SHIP, false);
+			api.addToFleet(FleetSide.ENEMY, "manticore_luddic_path_Strike", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "manticore_luddic_path_Strike", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "manticore_luddic_path_Strike", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "hammerhead_Balanced", FleetMemberType.SHIP, false);
@@ -32,7 +35,7 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 			api.addToFleet(FleetSide.ENEMY, "kite_luddic_path_Raider", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "kite_luddic_path_Raider", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "kite_luddic_path_Raider", FleetMemberType.SHIP, false);
-			api.addToFleet(FleetSide.ENEMY, "kite_luddic_path_Raider", FleetMemberType.SHIP, false);
+			api.addToFleet(FleetSide.ENEMY, "hound_luddic_path_Attack", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "hound_luddic_path_Attack", FleetMemberType.SHIP, false);
 			api.addToFleet(FleetSide.ENEMY, "hound_luddic_path_Attack", FleetMemberType.SHIP, false);
 
@@ -49,9 +52,9 @@ public class MissionDefinition implements MissionDefinitionPlugin {
 			   api.addNebula(x, y, radius);
 			}
 	  
-			api.addObjective(minX + width * 0.7F, minY + height * 0.25F, "sensor_array");
-			api.addObjective(minX + width * 0.8F, minY + height * 0.75F, "nav_buoy");
-			api.addObjective(minX + width * 0.2F, minY + height * 0.25F, "nav_buoy");
+			//api.addObjective(minX + width * 0.7F, minY + height * 0.25F, "sensor_array");
+			//api.addObjective(minX + width * 0.8F, minY + height * 0.75F, "nav_buoy");
+			//api.addObjective(minX + width * 0.2F, minY + height * 0.25F, "nav_buoy");
 	}
 
 }
